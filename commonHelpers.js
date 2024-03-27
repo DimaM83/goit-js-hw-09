a4033bcb085349625d2486055dc7fc0e4d8e38ea
DimaM83 <150599187+DimaM83@users.noreply.github.com>
2024-03-27 21:08:43 +0000

    Deploying to gh-pages from @ DimaM83/goit-js-hw-09@893eb21546f19b8f9e9ef6fac200c88a98f0e8b4 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/styles-97a090ec.js";console.log("Gallery");
+import"./assets/styles-97a090ec.js";import{S as o}from"./assets/vendor-c9def49e.js";new o(".gallery a",{});console.log(o);
 //# sourceMappingURL=commonHelpers.js.map
