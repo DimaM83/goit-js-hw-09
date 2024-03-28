9123b6887b78f669fdee0a557b81fb07c7df63e7
DimaM83 <mdv16.01.83@gmail.com>
2024-03-28 21:03:06 +0100

    add form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,37 @@
-console.log('Form');
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', event => {
+  const formData = {
+    email: form.email.value.trim(),
+    message: form.message.value.trim(),
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const savedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (savedFormData) {
+    form.email.value = savedFormData.email;
+    form.message.value = savedFormData.message;
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!form.email.value || !form.message.value) {
+    alert('Please fill in all fields');
+    return;
+  }
+
+  const formData = {
+    email: form.email.value.trim(),
+    message: form.message.value.trim(),
+  };
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+
+  form.reset();
+});
